e032383333552782c386ed807c696141d6c4a038
Cory Quammen <cquammen@mid-campus-03184.wireless.unc.edu>
2010-02-05 15:30:11 -0500

    Filled in much of the implementation for this class.

--- lib/Model/FluoroSim/ImportedPointSpreadFunction.cxx
+++ lib/Model/FluoroSim/ImportedPointSpreadFunction.cxx
@@ -1,16 +1,39 @@
 #include <ImportedPointSpreadFunction.h>
 
 #include <vtkAlgorithmOutput.h>
+#include <vtkImageChangeInformation.h>
+
+
+const std::string ImportedPointSpreadFunction::FILE_NAME_ATTRIBUTE  = "FileName";
+const std::string ImportedPointSpreadFunction::POINT_CENTER_ELEMENT = "PointCenter";
 
 
 ImportedPointSpreadFunction
 ::ImportedPointSpreadFunction() : PointSpreadFunction() {
+  m_ImageReader = ImageSourceType::New();
+
+  m_ITKToVTKFilter = new ITKImageToVTKImage<ImageType>();
+  m_ITKToVTKFilter->SetInput(m_ImageReader->GetOutput());
+
+  m_ChangeInformationFilter = vtkSmartPointer<vtkImageChangeInformation>::New();
+  m_ChangeInformationFilter->SetInputConnection(m_ITKToVTKFilter->GetOutputPort());
+
+  // Set up parameter names
+  m_ParameterNames.push_back("X Size (voxels)");
+  m_ParameterNames.push_back("Y Size (voxels)");
+  m_ParameterNames.push_back("Z Size (voxels)");
+  m_ParameterNames.push_back("X Voxel Spacing (nm)");
+  m_ParameterNames.push_back("Y Voxel Spacing (nm)");
+  m_ParameterNames.push_back("Z Voxel Spacing (nm)");
+  m_ParameterNames.push_back("X Point Center (nm)");
+  m_ParameterNames.push_back("Y Point Center (nm)");
+  m_ParameterNames.push_back("Z Point Center (nm)");
 }
 
 
 ImportedPointSpreadFunction
 ::~ImportedPointSpreadFunction() {
-
+  delete m_ITKToVTKFilter;
 }
 
 
@@ -18,6 +41,7 @@ void
 ImportedPointSpreadFunction
 ::SetFileName(const std::string& fileName) {
   m_FileName = fileName;
+  m_ImageReader->SetFileName(m_FileName);
 }
 
 
@@ -25,34 +49,6 @@ std::string
 ImportedPointSpreadFunction
 ::GetFileName() {
   return m_FileName;
-}
-
-
-void
-ImportedPointSpreadFunction
-::SetSpacing(double x, double y, double z) {
-
-}
-
-
-void
-ImportedPointSpreadFunction
-::GetSpacing(double& x, double& y, double& z) {
-
-}
-
-
-void
-ImportedPointSpreadFunction
-::SetOrigin(double x, double y, double z) {
-
-}
-
-
-void
-ImportedPointSpreadFunction
-::GetOrigin(double& x, double& y, double& z) {
-
 }
 
 
@@ -66,28 +62,33 @@ ImportedPointSpreadFunction
 vtkImageData*
 ImportedPointSpreadFunction
 ::GetOutput() {
-  return NULL;
+  return m_ChangeInformationFilter->GetOutput();
 }
 
 
 vtkAlgorithmOutput*
 ImportedPointSpreadFunction
 ::GetOutputPort() {
-  return NULL;
+  return m_ChangeInformationFilter->GetOutputPort();
 }
 
 
 int
 ImportedPointSpreadFunction
 ::GetNumberOfProperties() {
-  return 0;
+  return m_ParameterNames.size();
 }
 
 
 std::string
 ImportedPointSpreadFunction
 ::GetParameterName(int index) {
-  return std::string("Param");
+  try {
+    return m_ParameterNames.at(index);
+  } catch (...) {
+  }
+
+  return std::string("Error");
 }
 
 
@@ -108,13 +109,47 @@ ImportedPointSpreadFunction
 void
 ImportedPointSpreadFunction
 ::GetXMLConfiguration(xmlNodePtr node) {
+  xmlNodePtr root = xmlNewChild(node, NULL, BAD_CAST "ImportedPointSpreadFunction", NULL);
 
+  char doubleFormat[] = "%f";
+  char buf[128];
+
+  xmlNewProp(root, BAD_CAST NAME_ATTRIBUTE.c_str(), BAD_CAST m_Name.c_str());
+  xmlNewProp(root, BAD_CAST FILE_NAME_ATTRIBUTE.c_str(), BAD_CAST m_FileName.c_str());
+
+  vtkImageData* image = m_ITKToVTKFilter->GetOutput();
+
+  xmlNodePtr spacingNode = xmlNewChild(root, NULL, BAD_CAST SPACING_ELEMENT.c_str(), NULL);
+  sprintf(buf, doubleFormat, image->GetSpacing()[0]);
+  xmlNewProp(spacingNode, BAD_CAST X_ATTRIBUTE.c_str(), BAD_CAST buf);
+  sprintf(buf, doubleFormat, image->GetSpacing()[1]);
+  xmlNewProp(spacingNode, BAD_CAST Y_ATTRIBUTE.c_str(), BAD_CAST buf);
+  sprintf(buf, doubleFormat, image->GetSpacing()[2]);
+  xmlNewProp(spacingNode, BAD_CAST Z_ATTRIBUTE.c_str(), BAD_CAST buf);
+
+  xmlNodePtr pointCenterNode = xmlNewChild(root, NULL, BAD_CAST POINT_CENTER_ELEMENT.c_str(), NULL);
+  sprintf(buf, doubleFormat, m_PointCenter[0]);
+  xmlNewProp(pointCenterNode, BAD_CAST X_ATTRIBUTE.c_str(), BAD_CAST buf);
+  sprintf(buf, doubleFormat, m_PointCenter[1]);
+  xmlNewProp(pointCenterNode, BAD_CAST Y_ATTRIBUTE.c_str(), BAD_CAST buf);
+  sprintf(buf, doubleFormat, m_PointCenter[2]);
+  xmlNewProp(pointCenterNode, BAD_CAST Z_ATTRIBUTE.c_str(), BAD_CAST buf);
 }
 
 
 void
 ImportedPointSpreadFunction
 ::RestoreFromXML(xmlNodePtr node) {
+  const char* fileName = 
+    (const char*) xmlGetProp(node, BAD_CAST FILE_NAME_ATTRIBUTE.c_str());
+
+  m_ImageReader->SetFileName(fileName);
+  m_ImageReader->Update();
+}
 
 
+void
+ImportedPointSpreadFunction
+::RecenterImage() {
+  
 }
--- lib/Model/FluoroSim/ImportedPointSpreadFunction.h
+++ lib/Model/FluoroSim/ImportedPointSpreadFunction.h
@@ -1,25 +1,30 @@
 #ifndef _IMPORTED_POINT_SPREAD_FUNCTION_H_
 #define _IMPORTED_POINT_SPREAD_FUNCTION_H_
 
-#include <StdLibGuard.h>
-#include <string>
+#include <vector>
+
+#include <itkImageFileReader.h>
+#include <ITKImageToVTKImage.h>
+
+#include <vtkSmartPointer.h>
+
+class vtkImageChangeInformation;
 
 #include <PointSpreadFunction.h>
 
 
 class ImportedPointSpreadFunction : public PointSpreadFunction {
+
  public:
+  static const std::string FILE_NAME_ATTRIBUTE;
+  static const std::string POINT_CENTER_ELEMENT;
+
+
   ImportedPointSpreadFunction();
   virtual ~ImportedPointSpreadFunction();
 
   virtual void SetFileName(const std::string& fileName);
   virtual std::string GetFileName();
-
-  void SetSpacing(double  x, double  y, double  z);
-  void GetSpacing(double& x, double& y, double& z);
-
-  void SetOrigin(double  x, double  y, double  z);
-  void GetOrigin(double& x, double& y, double& z);
 
   void GetSize(int& ix, int& iy, int& iz);
 
@@ -34,8 +39,23 @@ class ImportedPointSpreadFunction : public PointSpreadFunction {
   virtual void GetXMLConfiguration(xmlNodePtr node);
   virtual void RestoreFromXML(xmlNodePtr node);
 
+  typedef float                                     PixelType;
+  typedef itk::Image<PixelType, 3>                  ImageType;
+  typedef itk::ImageFileReader<ImageType>           ImageSourceType;
+  typedef ImageSourceType::Pointer                  ImageSourceTypePointer;
+
  protected:
+  std::vector<std::string> m_ParameterNames;
+
   std::string m_FileName;
+  double m_PointCenter[3];
+
+  ImageSourceTypePointer m_ImageReader;
+  ITKImageToVTKImage<ImageType>* m_ITKToVTKFilter;
+
+  vtkSmartPointer<vtkImageChangeInformation> m_ChangeInformationFilter;
+
+  void RecenterImage();
 };
 
 
