ba5d7f4f4a12834787dad0c1b8d96476a5b6613d
Cory Quammen <cquammen@Macintosh-32.local>
2010-03-12 00:16:08 -0500

    Changed name from FluorescenceImageSourceInterface to FluorescenceImageSource.

---
+++ lib/Model/FluoroSim/FluorescenceImageSource.h
@@ -0,0 +1,37 @@
+#ifndef _FLUORESCENCE_IMAGE_SOURCE_H_
+#define _FLUORESCENCE_IMAGE_SOURCE_H_
+
+/* A fluorescence image source is responsible for generating and exporting
+   fluorescence images from a set of parameters. */
+
+// Forward declarations
+class vtkImageData;
+
+
+class FluorescenceImageSource {
+
+ public:
+  FluorescenceImageSource() {};
+  virtual ~FluorescenceImageSource() {};
+
+  // Exports a 2D fluorescence image
+  virtual vtkImageData* GenerateFluorescenceImage() = 0;
+
+  // Exports a stack of 2D fluorescence images (i.e. a 3D stack)
+  virtual vtkImageData* GenerateFluorescenceStackImage() = 0;
+
+  virtual int GetNumberOfParameters() = 0;
+
+  virtual void SetParameters(double* params) = 0;
+  virtual void GetParameters(double* params) = 0;
+
+  virtual int* GetDimensions() = 0;
+  virtual double* GetSpacing() = 0;
+
+ protected:
+  int    m_Dimensions[3];
+  double m_Spacing[3];
+
+};
+
+#endif // _FLUORESCENCE_IMAGE_SOURCE_H_
