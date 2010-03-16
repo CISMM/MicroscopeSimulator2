4a39293792ff18fc690f6649013b3e54c0ad3887
Cory Quammen <cquammen@Macintosh-32.local>
2010-03-15 23:27:10 -0400

    Initial check-in.

---
+++ lib/IO/VTKImageToITKImage.cxx
@@ -0,0 +1,89 @@
+#ifndef _VTK_IMAGE_TO_ITK_IMAGE_CXX_
+#define _VTK_IMAGE_TO_ITK_IMAGE_CXX_
+
+#include <VTKImageToITKImage.h>
+
+#include <vtkImageData.h>
+#include <vtkImageExport.h>
+
+
+template <class TImage>
+VTKImageToITKImage<TImage>
+::VTKImageToITKImage() {
+  m_VTKExporter = vtkImageExport::New();
+
+  m_ITKImporter = ImageImportType::New();
+  m_ITKImporter->
+    SetCallbackUserData(m_VTKExporter);
+  m_ITKImporter->
+    SetBufferPointerCallback(m_VTKExporter->GetBufferPointerCallback());
+  m_ITKImporter->
+    SetDataExtentCallback(m_VTKExporter->GetDataExtentCallback());
+  m_ITKImporter->
+    SetOriginCallback(m_VTKExporter->GetOriginCallback());
+  m_ITKImporter->
+    SetSpacingCallback(m_VTKExporter->GetSpacingCallback());
+  m_ITKImporter->
+    SetNumberOfComponentsCallback(m_VTKExporter->GetNumberOfComponentsCallback());
+  m_ITKImporter->
+    SetPipelineModifiedCallback(m_VTKExporter->GetPipelineModifiedCallback());
+  m_ITKImporter->
+    SetPropagateUpdateExtentCallback(m_VTKExporter->GetPropagateUpdateExtentCallback());
+  m_ITKImporter->
+    SetScalarTypeCallback(m_VTKExporter->GetScalarTypeCallback());
+  m_ITKImporter->
+    SetUpdateDataCallback(m_VTKExporter->GetUpdateDataCallback());
+  m_ITKImporter->
+    SetUpdateInformationCallback(m_VTKExporter->GetUpdateInformationCallback());
+  m_ITKImporter->
+    SetWholeExtentCallback(m_VTKExporter->GetWholeExtentCallback());
+}
+
+
+template <class TImage>
+VTKImageToITKImage<TImage>
+::~VTKImageToITKImage() {
+  m_VTKExporter->Delete();
+}
+
+
+template <class TImage>
+void
+VTKImageToITKImage<TImage>
+::SetInput(vtkImageData* image) {
+  m_VTKExporter->SetInput(image);
+}
+
+
+template <class TImage>
+void
+VTKImageToITKImage<TImage>
+::SetInputConnection(vtkAlgorithmOutput* input) {
+  m_VTKExporter->SetInputConnection(input);
+}
+
+
+template <class TImage>
+typename VTKImageToITKImage<TImage>::ImageTypePointer
+VTKImageToITKImage<TImage>
+::GetOutput() {
+  return m_ITKImporter->GetOutput();
+}
+
+
+template <class TImage>
+void
+VTKImageToITKImage<TImage>
+::Modified() {
+  m_ITKImporter->Modified();
+}
+
+
+template <class TImage>
+void
+VTKImageToITKImage<TImage>
+::Update() {
+  m_ITKImporter->Update();
+}
+
+#endif //  _VTK_IMAGE_TO_ITK_IMAGE_CXX_
---
+++ lib/IO/VTKImageToITKImage.h
@@ -0,0 +1,41 @@
+#ifndef _VTK_IMAGE_TO_ITK_IMAGE_H_
+#define _VTK_IMAGE_TO_ITK_IMAGE_H_
+
+#include <itkImage.h>
+#include <itkVTKImageImport.h>
+
+class vtkImageData;
+class vtkImageExport;
+class vtkAlgorithmOutput;
+
+
+template <class TImage>
+class VTKImageToITKImage {
+	
+ public:
+  typedef typename TImage::Pointer          ImageTypePointer;
+  typedef itk::VTKImageImport<TImage>       ImageImportType;
+  typedef typename ImageImportType::Pointer ImageImportTypePointer;
+
+  VTKImageToITKImage();
+  virtual ~VTKImageToITKImage();
+
+  void SetInput(vtkImageData* image);
+  void SetInputConnection(vtkAlgorithmOutput* input);
+
+  ImageTypePointer GetOutput();
+
+  void Modified();
+
+  void Update();
+	
+ protected:
+  ImageImportTypePointer m_ITKImporter;
+
+  vtkImageExport* m_VTKExporter;
+	
+};
+
+#include "VTKImageToITKImage.cxx"
+
+#endif // _VTK_IMAGE_TO_ITK_IMAGE_H_
