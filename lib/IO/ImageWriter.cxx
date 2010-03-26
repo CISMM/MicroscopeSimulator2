2723eb6af50c278f56e7d0963d058a20be84f8ba
Cory Quammen <cquammen@mid-campus-04347.wireless.unc.edu>
2010-03-26 15:52:45 -0400

    Initial check-in.

---
+++ lib/IO/ImageWriter.cxx
@@ -0,0 +1,67 @@
+#include <ImageWriter.h>
+
+#include <vtkAlgorithmOutput.h>
+
+
+ImageWriter
+::ImageWriter() {
+  m_FloatVTKToITKFilter = new VTKImageToITKImage<FloatImageType>();
+
+  m_FloatCaster = FloatCastFilterType::New();
+
+  m_UShortWriter = UShortWriterType::New();
+  m_UShortWriter->SetInput(m_FloatCaster->GetOutput());
+}
+
+
+ImageWriter
+::~ImageWriter() {
+  delete m_FloatVTKToITKFilter;
+}
+
+
+void
+ImageWriter
+::SetFileName(const std::string& fileName) {
+  m_UShortWriter->SetFileName(fileName);
+}
+
+
+std::string
+ImageWriter
+::GetFileName() const {
+  return m_UShortWriter->GetFileName();
+}
+
+
+void
+ImageWriter
+::SetImage(FloatImageType* image) {
+  m_FloatCaster->SetInput(image);
+}
+
+
+void
+ImageWriter
+::SetImage(vtkImageData* image) {
+  m_FloatVTKToITKFilter->SetInput(image);
+  m_FloatCaster->SetInput(m_FloatVTKToITKFilter->GetOutput());
+}
+
+
+void
+ImageWriter
+::SetInputConnection(vtkAlgorithmOutput* input) {
+  m_FloatVTKToITKFilter->SetInputConnection(input);
+  m_FloatCaster->SetInput(m_FloatVTKToITKFilter->GetOutput());
+}
+
+
+void
+ImageWriter
+::WriteUShortImage() {
+  m_UShortWriter->Write();
+}
+
+
+
---
+++ lib/IO/ImageWriter.h
@@ -0,0 +1,51 @@
+#ifndef _IMAGE_WRITER_H_
+#define _IMAGE_WRITER_H_
+
+#include <itkImage.h>
+#include <itkImageFileWriter.h>
+#include <itkShiftScaleImageFilter.h>
+
+#include <VTKImageToITKImage.h>
+
+
+class ImageWriter {
+
+ public:
+
+  // unsigned short image type
+  typedef unsigned short                  UShortPixelType;
+  typedef itk::Image<UShortPixelType, 3>  UShortImageType;
+  
+  typedef float                           FloatPixelType;
+  typedef itk::Image<FloatPixelType, 3>   FloatImageType;
+
+  typedef itk::ShiftScaleImageFilter<FloatImageType, UShortImageType> FloatCastFilterType;
+  typedef FloatCastFilterType::Pointer                                FloatCastFilterTypePointer;
+
+  typedef itk::ImageFileWriter<UShortImageType> UShortWriterType;
+  typedef UShortWriterType::Pointer             UShortWriterTypePointer;
+
+
+  ImageWriter();
+  virtual ~ImageWriter();
+
+  void SetFileName(const std::string& fileName);
+  std::string GetFileName() const;
+
+  void SetImage(FloatImageType* image);
+
+  void SetImage(vtkImageData* image);
+  void SetInputConnection(vtkAlgorithmOutput* input);
+
+  void WriteUShortImage();
+
+ protected:
+  VTKImageToITKImage<FloatImageType>* m_FloatVTKToITKFilter;
+
+  FloatCastFilterTypePointer m_FloatCaster;
+
+  UShortWriterTypePointer m_UShortWriter;
+};
+
+
+#endif // _IMAGE_WRITER_H_
