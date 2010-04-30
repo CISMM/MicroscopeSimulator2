184b761e5bc3b6e87139d325e7fa7ee435053288
Cory Quammen <cquammen@dhcp15101.odumvillagewireless.unc.edu>
2010-04-30 12:30:56 -0400

    Initial check-in.

---
+++ lib/Model/ModelObjects/PointRingModelObject.cxx
@@ -0,0 +1,104 @@
+#include <PointRingModelObject.h>
+#include <FluorophoreModelObjectProperty.h>
+
+#include <DirtyListener.h>
+#include <ModelObjectPropertyList.h>
+#include <XMLHelper.h>
+
+#include <vtkGlyph3D.h>
+#include <vtkPoints.h>
+#include <vtkPolyData.h>
+#include <vtkSphereSource.h>
+#include <vtkVertexGlyphFilter.h>
+
+
+const char* PointRingModelObject::OBJECT_TYPE_NAME = "PointRingModel";
+
+const char* PointRingModelObject::VISIBLE_RADIUS_PROP       = "Visible Radius";
+const char* PointRingModelObject::RING_RADIUS_PROP          = "Ring Radius";
+const char* PointRingModelObject::NUMBER_OF_POINTS_PROP     = "Number of Points";
+const char* PointRingModelObject::VERTICES_FLUOROPHORE_PROP = "Vertices Fluorophore Model";
+
+
+PointRingModelObject
+::PointRingModelObject(DirtyListener* dirtyListener) :
+  ModelObject(dirtyListener) {
+  m_ObjectTypeName = OBJECT_TYPE_NAME;
+  SetName("Point Ring");
+
+  // Set up geometry
+  m_Points = vtkSmartPointer<vtkPoints>::New();
+  m_Points->Allocate(10, 10);
+  
+  m_PointSource = vtkSmartPointer<vtkPolyData>::New();
+  m_PointSource->SetPoints(m_Points);
+  
+  m_SphereSource = vtkSmartPointer<vtkSphereSource>::New();
+  m_SphereSource->SetThetaResolution(16);
+  m_SphereSource->SetPhiResolution(8);
+
+  m_GlyphSource = vtkSmartPointer<vtkGlyph3D>::New();
+  m_GlyphSource->SetInput(m_PointSource);
+  m_GlyphSource->SetSourceConnection(m_SphereSource->GetOutputPort());
+
+  // Need a vertex source for the points so that the fluorescence renderer
+  // knows it has something to render.
+  m_VertexSource = vtkSmartPointer<vtkVertexGlyphFilter>::New();
+  m_VertexSource->SetInput(m_PointSource);
+
+  SetGeometrySubAssembly("All", m_GlyphSource);
+  SetGeometrySubAssembly("Points", m_VertexSource);
+
+  // Set up properties
+  AddProperty(new ModelObjectProperty(VISIBLE_RADIUS_PROP, 50.0, "nanometers", true, false));
+  AddProperty(new ModelObjectProperty(RING_RADIUS_PROP, 100.0, "nanometers", true, true));
+  AddProperty(new ModelObjectProperty(NUMBER_OF_POINTS_PROP, 5, "-", true, false));
+  AddProperty(new FluorophoreModelObjectProperty(VERTICES_FLUOROPHORE_PROP,
+                                                 GEOMETRY_VERTICES,
+                                                 m_VertexSource));
+
+  // Update again.
+  Update();
+}
+
+
+PointRingModelObject
+::PointRingModelObject(DirtyListener* dirtyListener, ModelObjectPropertyList* properties) 
+  : ModelObject(dirtyListener, properties) {
+}
+
+
+PointRingModelObject
+::~PointRingModelObject() {
+
+}
+
+
+void
+PointRingModelObject
+::Update() {
+  double visibleRadius = 
+    GetProperty(VISIBLE_RADIUS_PROP)->GetDoubleValue();
+  m_SphereSource->SetRadius(visibleRadius);
+
+  double ringRadius =
+    GetProperty(RING_RADIUS_PROP)->GetDoubleValue();
+  
+  int numPoints = 
+    GetProperty(NUMBER_OF_POINTS_PROP)->GetIntValue();
+
+  m_Points->SetNumberOfPoints(numPoints);
+
+  double angleIncrement = 2.0*M_PI / static_cast<double>(numPoints);
+  for (int i = 0; i < numPoints; i++) {
+    double xyz[3] = {0.0, 0.0, 0.0};
+    double angle = angleIncrement * static_cast<double>(i);
+    xyz[1] = ringRadius * cos(angle);
+    xyz[2] = ringRadius * sin(angle);
+    m_Points->SetPoint(static_cast<vtkIdType>(i), xyz);
+    m_Points->Modified();
+  }
+
+  GetGeometrySubAssembly("Points")->GetInput()->Modified();
+}
+
---
+++ lib/Model/ModelObjects/PointRingModelObject.h
@@ -0,0 +1,45 @@
+#ifndef _POINT_RING_MODEL_OBJECT_H_
+#define _POINT_RING_MODEL_OBJECT_H_
+
+#include <ModelObject.h>
+
+#include <vtkSmartPointer.h>
+
+class DirtyListener;
+class vtkGlyph3D;
+class vtkPoints;
+class vtkPolyData;
+class vtkSphereSource;
+class vtkVertexGlyphFilter;
+
+
+class PointRingModelObject : public ModelObject {
+
+ public:
+  static const char* OBJECT_TYPE_NAME;
+
+  static const char* VISIBLE_RADIUS_PROP;
+  static const char* RING_RADIUS_PROP;
+  static const char* NUMBER_OF_POINTS_PROP;
+  static const char* VERTICES_FLUOROPHORE_PROP;
+
+  
+  PointRingModelObject(DirtyListener* dirtyListener);
+  PointRingModelObject(DirtyListener* dirtyListener,
+                       ModelObjectPropertyList* properties);
+  virtual ~PointRingModelObject();
+
+  virtual void Update();
+
+ protected:
+  PointRingModelObject() {};
+
+  vtkSmartPointer<vtkPoints>            m_Points;
+  vtkSmartPointer<vtkPolyData>          m_PointSource;
+  vtkSmartPointer<vtkSphereSource>      m_SphereSource;
+  vtkSmartPointer<vtkGlyph3D>           m_GlyphSource;
+  vtkSmartPointer<vtkVertexGlyphFilter> m_VertexSource;
+
+};
+
+#endif //  _POINT_RING_MODEL_OBJECT_H_
