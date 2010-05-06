9589439f53b3ac96f18d55128c6c02d507c9c67e
Cory Quammen <cquammen@cpe-066-057-061-096.nc.res.rr.com>
2010-05-05 21:38:14 -0400

    Initial check-in.

---
+++ lib/Visualization/GeometryView/vtkImagePlaneWidgetRepresentation.cxx
@@ -0,0 +1,112 @@
+#include <vtkImagePlaneWidgetRepresentation.h>
+
+#include <vtkAbstractPicker.h>
+#include <vtkAbstractPropPicker.h>
+#include <vtkImageData.h>
+#include <vtkImagePlaneWidget.h>
+#include <vtkObjectFactory.h>
+#include <vtkProperty.h>
+#include <vtkRenderView.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+
+
+vtkCxxRevisionMacro(vtkImagePlaneWidgetRepresentation, "$Revision: 1.0 $");
+vtkStandardNewMacro(vtkImagePlaneWidgetRepresentation);
+
+
+vtkImagePlaneWidgetRepresentation
+::vtkImagePlaneWidgetRepresentation() {
+  this->SetNumberOfInputPorts(1);
+
+  this->XWidget = vtkSmartPointer<vtkImagePlaneWidget>::New();
+  this->XWidget->SetKeyPressActivationValue('x');
+  this->XWidget->RestrictPlaneToVolumeOn();
+  this->XWidget->GetPlaneProperty()->SetColor(1, 0, 0);
+  this->XWidget->TextureInterpolateOff();
+  this->XWidget->SetResliceInterpolateToNearestNeighbour();
+  this->XWidget->SetPlaneOrientationToXAxes();
+  this->XWidget->SetMarginSizeX(0.0);
+  this->XWidget->SetMarginSizeY(0.0);
+  this->XWidget->DisplayTextOn();
+
+  this->YWidget = vtkSmartPointer<vtkImagePlaneWidget>::New();
+  this->YWidget->SetKeyPressActivationValue('y');
+  this->YWidget->RestrictPlaneToVolumeOn();
+  this->YWidget->GetPlaneProperty()->SetColor(1, 0, 0);
+  this->YWidget->TextureInterpolateOff();
+  this->YWidget->SetResliceInterpolateToNearestNeighbour();
+  this->YWidget->SetPlaneOrientationToYAxes();
+  this->YWidget->SetMarginSizeX(0.0);
+  this->YWidget->SetMarginSizeY(0.0);
+  this->YWidget->DisplayTextOn();
+
+  this->ZWidget = vtkSmartPointer<vtkImagePlaneWidget>::New();
+  this->ZWidget->SetKeyPressActivationValue('z');
+  this->ZWidget->RestrictPlaneToVolumeOn();
+  this->ZWidget->GetPlaneProperty()->SetColor(1, 0, 0);
+  this->ZWidget->TextureInterpolateOff();
+  this->ZWidget->SetResliceInterpolateToNearestNeighbour();
+  this->ZWidget->SetPlaneOrientationToZAxes();
+  this->ZWidget->SetMarginSizeX(0.0);
+  this->ZWidget->SetMarginSizeY(0.0);
+  this->ZWidget->DisplayTextOn();
+}
+
+
+vtkImagePlaneWidgetRepresentation
+::~vtkImagePlaneWidgetRepresentation() {
+  
+}
+
+
+void
+vtkImagePlaneWidgetRepresentation
+::PrepareForRendering(vtkRenderView* view) {
+  vtkRenderWindowInteractor* interactor = view->GetRenderWindow()->GetInteractor();
+  if (!this->XWidget->GetInteractor()) {
+    this->XWidget->SetInteractor(interactor);
+    this->YWidget->SetInteractor(interactor);
+    this->ZWidget->SetInteractor(interactor);
+  }
+
+  if (this->GetInputDataObject(0,0)) {
+    this->XWidget->SetInput(dynamic_cast<vtkImageData*>(this->GetInputDataObject(0, 0)));
+    this->YWidget->SetInput(dynamic_cast<vtkImageData*>(this->GetInputDataObject(0, 0)));
+    this->ZWidget->SetInput(dynamic_cast<vtkImageData*>(this->GetInputDataObject(0, 0)));
+
+    vtkAbstractPropPicker* picker = dynamic_cast<vtkAbstractPropPicker*>(interactor->GetPicker());
+    this->XWidget->SetPicker(picker);
+    this->YWidget->SetPicker(picker);
+    this->ZWidget->SetPicker(picker);
+  }
+}
+
+
+bool
+vtkImagePlaneWidgetRepresentation
+::AddToView(vtkView* view) {
+  this->XWidget->On();
+  this->YWidget->On();
+  this->ZWidget->On();
+
+  return true;
+}
+
+
+bool
+vtkImagePlaneWidgetRepresentation
+::RemoveFromView(vtkView* view) {
+  this->XWidget->Off();
+  this->YWidget->Off();
+  this->ZWidget->Off();
+
+  return true;
+}
+
+
+void
+vtkImagePlaneWidgetRepresentation
+::PrintSelf(ostream& os, vtkIndent indent) {
+
+}
---
+++ lib/Visualization/GeometryView/vtkImagePlaneWidgetRepresentation.h
@@ -0,0 +1,41 @@
+#ifndef _VTK_IMAGE_PLANE_WIDGET_REPRESENTATION_H_
+#define _VTK_IMAGE_PLANE_WIDGET_REPRESENTATION_H_
+
+#include <vtkRenderedRepresentation.h>
+#include <vtkSmartPointer.h>
+
+// Forward declarations
+class vtkImagePlaneWidget;
+class vtkAbstractPicker;
+class Simulation;
+
+
+class vtkImagePlaneWidgetRepresentation : public vtkRenderedRepresentation {
+ public:
+  static vtkImagePlaneWidgetRepresentation *New();
+  vtkTypeRevisionMacro(vtkImagePlaneWidgetRepresentation, vtkRenderedRepresentation);
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+ protected:
+  vtkImagePlaneWidgetRepresentation();
+  virtual ~vtkImagePlaneWidgetRepresentation();
+
+  vtkSmartPointer<vtkAbstractPicker> Picker;
+
+  vtkSmartPointer<vtkImagePlaneWidget> XWidget;
+  vtkSmartPointer<vtkImagePlaneWidget> YWidget;
+  vtkSmartPointer<vtkImagePlaneWidget> ZWidget;
+
+  virtual void PrepareForRendering(vtkRenderView* view);
+
+  virtual bool AddToView(vtkView* view);
+  virtual bool RemoveFromView(vtkView* view);
+
+ private:
+  vtkImagePlaneWidgetRepresentation(const vtkImagePlaneWidgetRepresentation&);
+  void operator=(const vtkImagePlaneWidgetRepresentation&);
+
+};
+
+
+#endif // _VTK_IMAGE_PLANE_WIDGET_REPRESENTATION_H_
