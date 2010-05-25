879d2fc41dd662f4be3592efb4d664be30f6c8a7
Cory Quammen <cquammen@mid-campus-00105.wireless.unc.edu>
2010-05-25 19:02:44 -0400

    Made this an abstract base class for other fluorescence optimizers. As such, most of the functionality has been stripped out of it.

--- lib/Model/FluoroSim/FluorescenceOptimizer.cxx
+++ lib/Model/FluoroSim/FluorescenceOptimizer.cxx
@@ -1,14 +1,3 @@
-#include <itkAmoebaOptimizer.cxx>
-#include <itkExceptionObject.h>
-#include <itkFluorescenceImageSource.cxx>
-#include <itkGradientDescentOptimizer.cxx>
-#include <itkImage.txx>
-#include <itkImageToParameterizedImageSourceMetric.cxx>
-#include <itkMeanSquaresImageToImageMetric.txx>
-#include <itkNormalizedCorrelationImageToImageMetric.txx>
-#include <itkPoissonNoiseImageToImageMetric.cxx>
-#include <itkSingleValuedNonLinearOptimizer.cxx>
-
 #include <vtkImageData.h>
 
 #include <FluorescenceImageSource.h>
@@ -29,17 +18,6 @@ FluorescenceOptimizer
   m_FluoroSim = NULL;
   m_ModelObjectList = NULL;
   m_ComparisonImageModelObject = NULL;
-  
-  m_FluorescenceImageSource = SyntheticImageSourceType::New();
-
-  m_ImageToImageCostFunctionType = GAUSSIAN_NOISE_COST_FUNCTION;
-  m_OptimizerType = NELDER_MEAD_OPTIMIZER;
-
-  m_CostFunction = ParameterizedCostFunctionType::New();
-  
-  m_ImageToImageCostFunction = GaussianNoiseCostFunctionType::New();
-
-  SetUpOptimizer();
 }
 
 
@@ -67,15 +45,11 @@ FluorescenceOptimizer
 ::SetComparisonImageModelObjectIndex(int index) {
   if (index < 0) {
     m_ComparisonImageModelObject = NULL;
-    m_CostFunction->SetFixedImage(NULL);
     return;
   }
 
   ModelObjectPtr mo = m_ModelObjectList->GetModelObjectAtIndex(index, ImageModelObject::OBJECT_TYPE_NAME);
   m_ComparisonImageModelObject = static_cast<ImageModelObject*>(mo);
-
-  // Connect comparison image to optimization machinery
-  m_CostFunction->SetFixedImage(m_ComparisonImageModelObject->GetITKImage());
 }
 
 
@@ -83,100 +57,6 @@ ImageModelObject*
 FluorescenceOptimizer
 ::GetComparisonImageModelObject() {
   return m_ComparisonImageModelObject;
-}
-
-
-void
-FluorescenceOptimizer
-::Optimize() {
-
-  if (m_OptimizerType == POINTS_GRADIENT_DESCENT_OPTIMIZER) {
-
-    // TODO - set the cost function in the fluorescence image source. Currently,
-    // only the Gaussian and Poisson noise cost functions are supported.
-    
-
-    // TODO - figure out convergence criteria
-
-    for (int i = 0; i < 50; i++) {
-
-        // Compute the gradient. We don't need to read or process image data here.
-        // It is all done on the GPU.
-        VisualizationFluorescenceImageSource* imageSource =
-          dynamic_cast<VisualizationFluorescenceImageSource*>
-           (m_FluoroSim->GetFluorescenceImageSource());
-         imageSource->ComputePointsGradient();
-         
-         // Loop over the model objects. For each model object, get the
-         // gradient of its points and tell the model object about the gradient.
-         // We assume the model object will know how to adjust its parameters
-         // given the point gradient.
-         int numPoints;
-         float* gradientSrc = imageSource->
-           GetPointsGradientForModelObjectAtIndex(0, numPoints);
-         float* gradient = new float[3*numPoints];
-         memcpy(gradient, gradientSrc, sizeof(float)*3*numPoints);
-
-         // Scale the gradient
-         float t = 20.0;
-         for (int i = 0; i < numPoints; i++) {
-           gradient[3*i + 0] *= t;
-           gradient[3*i + 1] *= t;
-           gradient[3*i + 2] *= t;
-         }
-
-         m_ModelObjectList->GetModelObjectAtIndex(0)->
-           ApplySurfaceSampleForces(gradient);
-
-         delete[] gradient;
-     }
-
-
-  } else {
-
-    try {
-      
-      SetUpOptimizer();
-      
-      // Make sure to set the fluorescence image source and moving image.
-      m_FluorescenceImageSource->
-        SetFluorescenceImageSource(m_FluoroSim->GetFluorescenceImageSource());
-      
-      m_CostFunction->SetMovingImageSource(m_FluorescenceImageSource);
-      
-      typedef ParameterizedCostFunctionType::ParametersMaskType
-        ParametersMaskType;
-      ParametersMaskType* mask = m_CostFunction->GetParametersMask();
-      
-      // Pluck out the active parameters
-      typedef ParameterizedCostFunctionType::ParametersType ParametersType;
-      ParametersType activeParameters
-        = ParametersType(m_CostFunction->GetNumberOfParameters());
-      int activeIndex = 0;
-      for (unsigned int i = 0; i < mask->Size(); i++) {
-        if (mask->GetElement(i)) {
-          // TODO - The right hand side is slower than it has to be. Make it faster
-          activeParameters[activeIndex++] = m_FluorescenceImageSource->GetParameters()[i];
-        }
-      }
-      
-      std::cout << "Starting parameters: " << activeParameters << std::endl;
-      
-      // Connect to the cost function, set the initial parameters, and optimize.
-      m_ImageToImageCostFunction
-        ->SetFixedImageRegion(m_FluorescenceImageSource->GetOutput()->GetLargestPossibleRegion());
-      
-      m_CostFunction->SetImageToImageMetric(m_ImageToImageCostFunction);
-      
-      m_Optimizer->SetCostFunction(m_CostFunction);
-      m_Optimizer->SetInitialPosition(activeParameters);
-      m_Optimizer->StartOptimization();
-      
-    } catch (itk::ExceptionObject exception) {
-      std::cout << "Optimizer exception: " << exception.GetDescription() << std::endl;
-    }
-    
-  }
 }
 
 
@@ -190,33 +70,25 @@ FluorescenceOptimizer
 }
 
 
+int
+FluorescenceOptimizer
+::GetNumberOfAvailableObjectiveFunctions() const {
+  return m_ObjectiveFunctionNames.size();
+}
+
+
+std::string
+FluorescenceOptimizer
+::GetAvailableObjectiveFunctionName(int index) {
+  if (index < 0 || index > 2)
+    return std::string();
+
+  return m_ObjectiveFunctionNames[index];
+}
+
+
 void
 FluorescenceOptimizer
-::SetUpOptimizer() {
-
-  // Set up the cost function
-  if (m_ImageToImageCostFunctionType == GAUSSIAN_NOISE_COST_FUNCTION) {
-    m_ImageToImageCostFunction = GaussianNoiseCostFunctionType::New();
-    std::cout << "Gaussian noise cost function, ";
-  } else if (m_ImageToImageCostFunctionType == POISSON_NOISE_COST_FUNCTION) {
-    m_ImageToImageCostFunction = PoissonNoiseCostFunctionType::New();
-    std::cout << "Poisson noise cost function, ";
-  } else if (m_ImageToImageCostFunctionType == NORMALIZED_CORRELATION_COST_FUNCTION) {
-    m_ImageToImageCostFunction = NormalizedCorrelationCostFunctionType::New();
-    std::cout << "Normalized correlation cost function, ";
-  }
-
-  // Set up the optimizer
-  if (m_OptimizerType == NELDER_MEAD_OPTIMIZER) {
-    NelderMeadOptimizerType::Pointer optimizer = NelderMeadOptimizerType::New();
-    optimizer->SetFunctionConvergenceTolerance(1e-3);
-    m_Optimizer = optimizer;
-    std::cout << "Nelder-Mead optimizer";
-  } else if (m_OptimizerType == GRADIENT_DESCENT_OPTIMIZER) {
-    GradientDescentOptimizerType::Pointer optimizer = GradientDescentOptimizerType::New();
-    m_Optimizer = optimizer;
-    std::cout << "Gradient descent optimizer";
-  }
-
-  std::cout << std::endl;
+::AddObjectiveFunctionName(const std::string& name) {
+  m_ObjectiveFunctionNames.push_back(name);
 }
--- lib/Model/FluoroSim/FluorescenceOptimizer.h
+++ lib/Model/FluoroSim/FluorescenceOptimizer.h
@@ -1,62 +1,16 @@
 #ifndef _FLUORESCENCE_OPTIMIZER_H_
 #define _FLUORESCENCE_OPTIMIZER_H_
 
-
-#define ITK_MANUAL_INSTANTIATION
-#include <itkAmoebaOptimizer.h>
-#include <itkFluorescenceImageSource.h>
-#include <itkGradientDescentOptimizer.h>
-#include <itkImage.h>
-#include <itkImageToParameterizedImageSourceMetric.h>
-#include <itkMeanSquaresImageToImageMetric.h>
-#include <itkNormalizedCorrelationImageToImageMetric.h>
-#include <itkPoissonNoiseImageToImageMetric.h>
-#include <itkSingleValuedNonLinearOptimizer.h>
-#undef ITK_MANUAL_INSTANTIATION
-
+#include <string>
+#include <vector>
 
 class FluorescenceSimulation;
 class ImageModelObject;
 class ModelObjectList;
 
-
 class FluorescenceOptimizer {
 
  public:
-
-  typedef enum {
-    GAUSSIAN_NOISE_COST_FUNCTION,
-    POISSON_NOISE_COST_FUNCTION,
-    NORMALIZED_CORRELATION_COST_FUNCTION
-  } CostFunction_t;
-
-  typedef enum {
-    NELDER_MEAD_OPTIMIZER,
-    GRADIENT_DESCENT_OPTIMIZER,
-    POINTS_GRADIENT_DESCENT_OPTIMIZER
-  } Optimizer_t;
-
-  typedef float PixelType;
-  typedef itk::Image<PixelType, 3> FluorescenceImageType;
-  typedef itk::FluorescenceImageSource<FluorescenceImageType> SyntheticImageSourceType;
-
-  // Types for optimization.
-  typedef itk::ImageToParameterizedImageSourceMetric<FluorescenceImageType, SyntheticImageSourceType>
-    ParameterizedCostFunctionType;
-
-  typedef itk::ImageToImageMetric<FluorescenceImageType, FluorescenceImageType>
-    ImageToImageCostFunctionType;
-  typedef itk::MeanSquaresImageToImageMetric<FluorescenceImageType, FluorescenceImageType>
-    GaussianNoiseCostFunctionType;
-  typedef itk::PoissonNoiseImageToImageMetric<FluorescenceImageType, FluorescenceImageType>
-    PoissonNoiseCostFunctionType;
-  typedef itk::NormalizedCorrelationImageToImageMetric<FluorescenceImageType, FluorescenceImageType>
-    NormalizedCorrelationCostFunctionType;
-    
-  typedef itk::SingleValuedNonLinearOptimizer OptimizerType;
-  typedef itk::AmoebaOptimizer               NelderMeadOptimizerType;
-  typedef itk::GradientDescentOptimizer      GradientDescentOptimizerType;
-
 
   /** Constructor/destructor. */
   FluorescenceOptimizer();
@@ -68,33 +22,12 @@ class FluorescenceOptimizer {
   void SetComparisonImageModelObjectIndex(int index);
   ImageModelObject* GetComparisonImageModelObject();
 
-  void SetCostFunctionToGaussianNoise() {
-    m_ImageToImageCostFunctionType = GAUSSIAN_NOISE_COST_FUNCTION;
-  }
-
-  void SetCostFunctionToPoissonNoise() {
-    m_ImageToImageCostFunctionType = POISSON_NOISE_COST_FUNCTION;
-  }
-
-  void SetCostFunctionToNormalizedCorrelation() {
-    m_ImageToImageCostFunctionType = NORMALIZED_CORRELATION_COST_FUNCTION;
-  }
-
-  void SetOptimizerToNelderMead() {
-    m_OptimizerType = NELDER_MEAD_OPTIMIZER;
-  }
-
-  void SetOptimizerToGradientDescent() {
-    m_OptimizerType = GRADIENT_DESCENT_OPTIMIZER;
-  }
-
-  void SetOptimizerToPointsGradientDescent() {
-    m_OptimizerType = POINTS_GRADIENT_DESCENT_OPTIMIZER;
-  }
-
-  void Optimize();
+  virtual void Optimize() = 0;
 
   void SetParameters(double* params);
+
+  virtual int GetNumberOfAvailableObjectiveFunctions() const;
+  virtual std::string GetAvailableObjectiveFunctionName(int index);
 
  protected:
   FluorescenceSimulation* m_FluoroSim;
@@ -102,22 +35,13 @@ class FluorescenceOptimizer {
   ModelObjectList* m_ModelObjectList;
   ImageModelObject* m_ComparisonImageModelObject;
 
-  SyntheticImageSourceType::Pointer m_FluorescenceImageSource;
+  // List of available objective functions
+  std::vector<std::string> m_ObjectiveFunctionNames;
 
-  // The cost function used by the optimizer
-  ParameterizedCostFunctionType::Pointer m_CostFunction;
+  // The chosen objective function
+  std::string m_ObjectiveFunction;
 
-  // The delegate cost function used by m_CostFunction
-  ImageToImageCostFunctionType::Pointer  m_ImageToImageCostFunction;
-
-  CostFunction_t m_ImageToImageCostFunctionType;
-  
-  // The optimizer
-  OptimizerType::Pointer m_Optimizer;
-
-  Optimizer_t m_OptimizerType;
-
-  void SetUpOptimizer();
+  void AddObjectiveFunctionName(const std::string& name);
 
 };
 
